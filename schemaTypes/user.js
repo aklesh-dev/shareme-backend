77ed108512594d583a8c91b3ca5fd1c714a3e858
aklesh yadav <arzunayavadu998@gmail.com>
2024-08-14 15:28:52 +0545

    first commit

---
+++ schemaTypes/user.js
@@ -0,0 +1,19 @@
+export const user = {
+    name: 'user',
+    title: 'User',
+    type: 'document',
+    fields: [
+        {
+            name: 'username',
+            title: 'Username',
+            type: 'string',
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'string',
+        },
+        
+    ]
+};
+
